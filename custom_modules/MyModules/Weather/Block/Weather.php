99407b88e619be21d2cccec76ec092a2d9fbdf87
root <root@vps-23-12-06-21-43-34-287.novalocal>
2023-12-14 23:55:43 +0700

    added magento source code and database

---
+++ custom_modules/MyModules/Weather/Block/Weather.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MyModules\Weather\Block;
+
+class Weather extends \Magento\Framework\View\Element\Template
+{
+    protected function _prepareLayout()
+    {
+        return parent::_prepareLayout();
+    }
+}
